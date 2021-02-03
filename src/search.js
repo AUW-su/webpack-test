ecbc3b21f9bc312c75d23c41b6b4c1048a7dbf34
wenxiu.su <wenxiu.su@beibei.com>
2021-02-03 20:04:10 +0800

    1

--- src/search.js
+++ src/search.js
@@ -1,11 +1,12 @@
 'use strict'
 
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react';
+import ReactDom from 'react-dom';
+import './search.css'
 
 class Search extends React.Component {
     render() {
-        return <div>Search Text</div>;
+        return <div className="search-text">Search Text</div>;
     }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,18 @@ module.exports = {
         filename: '[name].js', // name 占位符
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            use: "babel-loader",
-        }]
-
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ],
     }
 }
